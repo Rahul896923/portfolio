a0a88bb21efe9a434763706720991ee3839c3344
Rahul Kumar Ray <139076979+Rahul896923@users.noreply.github.com>
2023-07-16 13:04:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var typed = new Typed('#text', {
+    strings: ['ER Rahul Kumar Ray...', '+91- 8969235410...', 'Full Stack Developer...', 'Web Developer...', 'Fronted Developer...', 'Backend Developer...'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
+
+
+
+
+
+
+
+
